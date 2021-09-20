3911376a5ceb1ecb6a5eafc3e645719d5b5c34ec
Matthieu Van Wittenberghe <mvanwit@gmail.com>
2021-09-20 21:53:46 +0200

    Copy extracted dates and validation

--- dateDetection.py
+++ dateDetection.py
@@ -5,12 +5,13 @@ import re, pyperclip
 # Create a regex object for dates in the DD/MM/YYYY format
 
 dateRegex = re.compile(r''' 
-
+	(
 	(0[1-9] | [12]\d | 3[01])	# Days from 01 to 31
 	/							# / separator
 	(0[1-9] | 1[0-2])			# Months from 01 to 12
 	/							# / separator
-	([12]\d\d\d)				# Year from 1000 to 2999
+	([12]\d\d\d)
+	)				# Year from 1000 to 2999
 	''', re.VERBOSE)
 
 # Get the text off the clipboard
@@ -23,19 +24,21 @@ extractedDates = dateRegex.findall(text)
 
 # Store these strings in variables "day" "month" "year"
 
+allDates = []
 day = []
 month = []
 year = []
 
 for dates in extractedDates:
-	day.append(int(dates[0]))
-	month.append(int(dates[1]))
-	year.append(int(dates[2]))
+	allDates.append(dates[0])
+	day.append(dates[1])
+	month.append(dates[2])
+	year.append(dates[3])
 
 # Detect if a date is valid
 
 isValid = []
-30daysMonths = [04, 06, 09, 11] #April, June, September, November have 30 days
+thirtyMonths = ['04', '06', '09', '11'] #April, June, September, November have 30 days
 
 	# Function that detects if a year is a leap year
 def isLeapYear(someYear):
@@ -46,16 +49,22 @@ def isLeapYear(someYear):
 	else:
 		return False
 
-for i in range(len(day)):
-	if month[i] in 30daysMonths and day[i] == 31: #31 days in a 30 days month
+combinedDateValidation = []
+for i in range(len(allDates)):
+	if month[i] in thirtyMonths and int(day[i]) == 31: #31 days in a 30 days month
 		isValid.append('not valid')
-	elif month[i] == 02 and day[i] > 29: #more than 29 days in February
+	elif month[i] == '02' and int(day[i]) > 29: #more than 29 days in February
 		isValid.append('not valid')
-	elif month[i] == 02 and day[i] == 29 and not isLeapYear(year[i]) #29 days in February not on a leap year
+	elif month[i] == '02' and int(day[i]) == 29 and not isLeapYear(int(year[i])): #29 days in February not on a leap year
 		isValid.append('not valid')
 	else:
 		isValid.append('valid')
 
+# Copy the extracted dates and the validation infos to the clipboard	
+	combinedDateValidation.append(allDates[i] + '   ' + isValid[i])
 
 
-#TODO: Copy the extracted dates and the validation infos to the clipboard
+	
+result = '\n'.join(combinedDateValidation)
+
+pyperclip.copy(result)
