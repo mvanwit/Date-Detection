227401220c672991a87ceb096451a7761562d7da
Matthieu Van Wittenberghe <mvanwit@gmail.com>
2021-09-20 12:22:31 +0200

    Create dateDetection.py
    Skeleton of the program

---
+++ dateDetection.py
@@ -0,0 +1,13 @@
+#! python3
+
+#TODO: Create a regex object for dates in the DD/MM/YYYY format
+
+#TODO: Get the text off the clipboard
+
+#TODO: Extract the dates from this text
+
+#TODO: Store these strings in variables "day" "month" "year"
+
+#TODO: Create a function that can detect if a date is valid
+
+#TODO: Copy the extracted dates and the validation infos to the clipboard
