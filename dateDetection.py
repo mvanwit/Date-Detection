414aaf2876a658e849408370643fcd5d454fe152
Matthieu Van Wittenberghe <mvanwit@gmail.com>
2021-09-20 18:30:09 +0200

    Create dateRegex

--- dateDetection.py
+++ dateDetection.py
@@ -2,15 +2,15 @@
 
 import re, pyperclip
 
-#TODO: Create a regex object for dates in the DD/MM/YYYY format
+# Create a regex object for dates in the DD/MM/YYYY format
 
 dateRegex = re.compile(r''' 
 
-		# Days from 01 to 31
-	/	# / separator
-		# Months from 01 to 12
-	/	# / separator
-		# Year from 1000 to 2999
+	(0[1-9] | [12]\d | 3[01])	# Days from 01 to 31
+	/							# / separator
+	(0[1-9] | 1[0-2])			# Months from 01 to 12
+	/							# / separator
+	([12]\d\d\d)				# Year from 1000 to 2999
 	''', re.VERBOSE)
 
 #TODO: Get the text off the clipboard
